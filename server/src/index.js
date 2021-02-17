c59d0556322c74f6a621f90bbea542929efad839
Seongsoo Yim <seongsooyim@192-168-1-100.tpgi.com.au>
2021-02-17 17:15:52 +1100

    Depl

--- server/src/index.js
+++ server/src/index.js
@@ -6,5 +6,5 @@ const port = process.env.PORT || 5000
 // * This file will be used to run development mode by listening:3000
 // * as reuire(app) containing express and mongoDB 
 app.listen(port, () => {
-    console.log('Server up.........: ' + port)
+    console.log('Server up..........: ' + port)
 })
